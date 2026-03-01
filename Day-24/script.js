a0d51c2d565101ec9fe6d75042d907014f0c0827
sumayya26 <23r01a05j4@cmrithyderabad.edu.in>
2026-03-01 15:30:05 +0530

    🚀 Day 24 - index.html - 15:30:00

---
+++ Day-24/script.js
@@ -0,0 +1,7 @@
+// Day 24 Logic Practice
+console.log('Lab Session 24 Started');
+
+const display = document.getElementById('display');
+display.innerText = 'Logic Engine Online';
+
+// Practice your JS code here...
